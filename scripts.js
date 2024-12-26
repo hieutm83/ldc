217a0ffc325d0075e5a7f514a55ac3e823424196
hieutm83 <tolahatdoxanh@gmail.com>
2024-12-26 11:56:25 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,19 @@
 function toggleSubdirectory(subdirectoryId, itemId) {
-  // Lấy subdirectory và item tương ứng
   const subdirectory = document.getElementById(subdirectoryId);
   const item = document.getElementById(itemId);
+
+  console.log('Subdirectory:', subdirectory);
+  console.log('Item:', item);
 
   if (subdirectory) {
     const isHidden = subdirectory.classList.contains('hidden');
 
-    // Luôn ẩn tất cả các subdirectory và loại bỏ class 'active' trên các item
     document.querySelectorAll('[id^="subdirectory"]').forEach((sub) => {
       sub.classList.add('hidden');
       sub.classList.remove('open');
     });
     document.querySelectorAll('[id^="item"]').forEach((item) => item.classList.remove('active'));
 
-    // Nếu subdirectory đang ẩn, hiển thị nó và thêm class 'active' vào item tương ứng
     if (isHidden) {
       subdirectory.classList.remove('hidden');
       subdirectory.classList.add('open');
